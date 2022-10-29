df0cc3ccb6c17d86fbb5c06d0703873dc9d5f5d5
Syncxv <aruldevarackal1@gmail.com>
2022-10-29 06:57:59 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+    return (
+        <div>
+            <span>hi</span>
+        </div>
+    );
+};
+
+export default App;
