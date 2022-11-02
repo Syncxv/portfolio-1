6e8ec33965207da05e16fa6371f3663dab2947bd
Syncxv <aruldevarackal1@gmail.com>
2022-11-02 21:49:11 +0530

    :O looks promising eh

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,18 +1,40 @@
-import { createSignal } from 'solid-js';
-import NavPage from '../NavPage';
+import { stagger } from 'motion';
+import { createAnimation } from 'motion-signals';
+import { createSignal, onMount } from 'solid-js';
 import Styles from './NavBar.module.scss';
 function NavBar() {
     const [isOpen, setOpen] = createSignal(false);
+    const { play, getIsFinished, replay, reset, getAnimateInstance } = createAnimation(
+        `.${Styles.bar}`,
+        { y: ['-100vh', 0], opacity: 1 },
+
+        {
+            delay: stagger(0.3),
+            duration: 2,
+            easing: [0.22, 0.03, 0.26, 1],
+        }
+    );
+
     return (
         <>
-            <nav class={Styles.nav} onClick={() => setOpen((prev) => !prev)}>
+            <nav
+                class={Styles.nav}
+                onClick={() => {
+                    isOpen() ? getAnimateInstance()?.reverse() : play();
+                    setOpen((prev) => !prev);
+                }}
+            >
                 <button class={Styles.navButton}>
                     <div class={Styles.line1}></div>
                     <div class={Styles.line2}></div>
                     <div class={Styles.line3}></div>
                 </button>
             </nav>
-            <NavPage isOpen={isOpen} />
+            <div class={Styles.Bars}>
+                <div class={Styles.bar}></div>
+                <div class={Styles.bar}></div>
+                <div class={Styles.bar}></div>
+            </div>
         </>
     );
 }
