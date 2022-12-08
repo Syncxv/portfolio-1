a9224097d41dac9b60f478c1bfbc2cbde3f244a5
Syncxv <aruldevarackal1@gmail.com>
2022-12-09 01:48:00 +1100

    router init

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@
 import 'windi.css';
 import './scss/_main.scss';
 import { render } from 'solid-js/web';
-
+import { Router } from '@solidjs/router';
 import App from './App';
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(
+    () => (
+        <Router>
+            <App />
+        </Router>
+    ),
+    document.getElementById('root') as HTMLElement
+);
